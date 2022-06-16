d56bbfabf49be3061d3aafd85a6cef4a2ccf4ef3
vignesh-2706 <86919953+vignesh-2706@users.noreply.github.com>
2022-06-16 12:03:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#importng the libraries
 from flask import Flask, request, render_template
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
-import pickle
 
-model = pickle.load(open('RA.pickle','rb'))  #loading the model
+model = load_model('SavedModel_V2.h5')  #loading the model
 class_names = ['Negative', 'Positive']  #assigning the class_names
 
 app = Flask(__name__)
@@ -13,7 +11,7 @@ app = Flask(__name__)
 #funtion to predict the image
 def predict(image):
     img = Image.open(image)     #reading the image
-    img = img.resize((256,256))     #resizing the image
+    img = img.resize((180,180))     #resizing the image
     img = np.array(img)     #converting the image to numpy array
     img = np.expand_dims(img, axis=0)   #expanding the dimension
     img = img[:,:,:,0:3]     #converting 4 channel to 3 channel
